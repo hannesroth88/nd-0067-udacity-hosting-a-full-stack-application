0126ab91802130a309b9abe405abc39fd93f3ff8
unknown <hannes.roth88@gmail.com>
2022-03-31 01:59:14 +0200

    changed to Typescript

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -4,7 +4,7 @@ import axiosRetry from "axios-retry";
 const RestaurantFinder = axios.create({
   baseURL:
     process.env.NODE_ENV !== "production"
-      ? "http://localhost:7000/api/v1/restaurants"
+      ? "http://localhost:4200/api/v1/restaurants"
       : "https://alluring-bryce-canyon-75245.herokuapp.com/api/v1/restaurants",
   timeout: 5000,
 });
