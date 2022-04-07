eaec82fe5eb6220fa6d9c1d6e046d005cf59ba00
unknown <hannes.roth88@gmail.com>
2022-04-07 22:12:20 +0200

    updated config

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,13 +1,10 @@
 import { Pool, QueryArrayConfig } from "pg"
 import "dotenv/config"
 
-const isProduction = process.env.NODE_ENV === "production"
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+const connectionString = `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`
 const pool = new Pool({
-  connectionString: isProduction
-    ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
-    : connectionString,
-  ssl: isProduction ? { rejectUnauthorized: false } : false
+  connectionString: connectionString,
+  ssl: process.env.NODE_ENV == 'prod' ? { rejectUnauthorized: false } : false
 })
 
 module.exports = {
