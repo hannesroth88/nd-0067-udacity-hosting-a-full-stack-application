b4ec57f2262106aa73bc283dac7afc515bd0af2d
unknown <hannes.roth88@gmail.com>
2022-04-08 02:28:12 +0200

    seperated backend and frontent build

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -3,7 +3,7 @@ import axiosRetry from "axios-retry";
 
 const RestaurantFinder = axios.create({
   baseURL:
-    process.env.NODE_ENV !== "prod"
+    process.env.NODE_ENV !== "PROD"
       ? "http://localhost:4200/api/v1/restaurants"
       : "https://udacity-fullstack-backend-env.eba-up9w3ehf.us-east-1.elasticbeanstalk.com/api/v1/restaurants",
   timeout: 5000,
--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -4,7 +4,7 @@ import "dotenv/config"
 const connectionString = `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`
 const pool = new Pool({
   connectionString: connectionString,
-  ssl: process.env.NODE_ENV == 'prod' ? { rejectUnauthorized: false } : false
+  ssl: process.env.NODE_ENV == 'PROD' ? { rejectUnauthorized: false } : false
 })
 
 module.exports = {
