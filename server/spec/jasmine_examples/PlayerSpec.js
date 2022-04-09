71af41395a1920a451efaf8204999a225f801bcd
unknown <hannes.roth88@gmail.com>
2022-04-09 21:18:23 +0200

    added tests

--- server/spec/jasmine_examples/PlayerSpec.js
+++ server/spec/jasmine_examples/PlayerSpec.js
@@ -11,8 +11,7 @@ describe("Player", function() {
 
   it("should be able to play a Song", function() {
     player.play(song);
-    // expect(player.currentlyPlayingSong).toEqual(song);
-    expect(player.currentlyPlayingSong).toEqual("wrong");
+    expect(player.currentlyPlayingSong).toEqual(song);
 
     //demonstrates use of custom matcher
     expect(player).toBePlaying(song);
