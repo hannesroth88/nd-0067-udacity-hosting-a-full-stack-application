3bb75530148e761215d3793f5b28cb7dddabb6c4
unknown <hannes.roth88@gmail.com>
2022-04-09 12:34:26 +0200

    added prod env to start script

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -3,7 +3,7 @@ import axiosRetry from "axios-retry";
 
 const RestaurantFinder = axios.create({
   baseURL:
-    process.env.NODE_ENV !== "PROD"
+    process.env.NODE_ENV !== "production"
       ? "http://localhost:4200/api/v1/restaurants"
       : "http://udacity-fullstack-backend-env.eba-up9w3ehf.us-east-1.elasticbeanstalk.com/api/v1/restaurants",
   timeout: 5000,
